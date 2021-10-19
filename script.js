c292f90f07091e6692a269e7d424908353f423cc
Violetta Szewczyk <szewczykviolet@gmail.com>
2021-10-18 23:07:32 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function likeCount(){
+    document.getElementById('count').innerText++;
+}
+
+function secondLike(){
+    document.getElementById('count2').innerText++;
+}
+
+function thirdLike(){
+    document.getElementById('count3').innerText++;
+}
